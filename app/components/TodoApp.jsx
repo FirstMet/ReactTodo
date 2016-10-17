e1c46478a4f0737be6cb98ce2b88128b3e26fa8d
Fragments <firstmetgithub@gmail.com>
2016-10-17 21:59:50 +0800

    Add TodoApp component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoApp extends React.Component{
+    render() {
+        return(
+            <div>
+                TodoApp
+            </div>
+        )
+    }
+}
+
+export default TodoApp;
