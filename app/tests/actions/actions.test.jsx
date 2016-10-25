a89223939f1f3a79c16144121186a2ed255e65db
Fragments <firstmetgithub@gmail.com>
2016-10-25 22:07:45 +0800

    Update todos on firebase

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 var expect = require('expect');
 
+import firebase, {firebaseRef} from 'app/firebase/';
 var actions = require('actions');
 
 var createMockStore = configureMockStore([thunk]);
@@ -43,13 +44,32 @@ describe('Actions', () => {
         expect(res).toEqual(action);
     });
 
-    it('should generate toggle todo action', () => {
+    it('should create todo and dispatch ADD_TODO', (done) => {
+        const store = createMockStore({});
+        const todoText = 'My todo item';
+
+        store.dispatch(actions.startAddTodo(todoText)).then(() => {
+            const actions = store.getActions();
+            expect(actions[0]).toInclude({
+                type: 'ADD_TODO'
+            });
+
+            expect(actions[0].todo).toInclude({
+                text: todoText
+            });
+            done();
+        }).catch(done);
+
+    });
+
+    it('should generate update todo action', () => {
         var action = {
-            type: 'TOGGLE_TODO',
-            id: '1'
+            type: 'UPDATE_TODO',
+            id: '1',
+            updates: {completed: false}
         };
 
-        var res = actions.toggleTodo(action.id);
+        var res = actions.updateTodo(action.id, action.updates);
 
         expect(res).toEqual(action);
     });
@@ -73,21 +93,44 @@ describe('Actions', () => {
         expect(res).toEqual(action);
     });
 
-    it('should create todo and dispatch ADD_TODO', (done) => {
-        const store = createMockStore({});
-        const todoText = 'My todo item';
+    describe('Tests with firebase todos', () => {
+        var testTodoRef;
 
-        store.dispatch(actions.startAddTodo(todoText)).then(() => {
-            const actions = store.getActions();
-            expect(actions[0]).toInclude({
-                type: 'ADD_TODO'
-            });
+        beforeEach((done) => {
+            testTodoRef = firebaseRef.child('todos').push();
+            testTodoRef.set({
+                text: 'Some todo',
+                completed: false,
+                createdAt: 100
+            }).then(() => done());
+        });
 
-            expect(actions[0].todo).toInclude({
-                text: todoText
-            });
-            done();
-        }).catch(done);
+        afterEach((done) => {
+            testTodoRef.remove().then(() => done());
+        });
+
+        it('should toggle todo and dispatch UPDATE_TODO action', (done) => {
+            const store = createMockStore({});
+            const action = actions.startToggleTodo(testTodoRef.key, true);
+
+            store.dispatch(action).then(() => {
+                const mockActions = store.getActions();
+
+                expect(mockActions[0]).toInclude({
+                    type: 'UPDATE_TODO',
+                    id: testTodoRef.key,
+                });
+
+                expect(mockActions[0].updates).toInclude({
+                    completed: true
+                });
+
+                expect(mockActions[0].updates.completedAt).toExist();
+
+                done();
+
+            }, done);
+        });
 
     });
 
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -52,7 +52,7 @@ describe('Reducers', () => {
 
         });
 
-        it('should toggle todo', () => {
+        it('should update todo', () => {
             var todos = [{
                 id: 1,
                 text: 'something',
@@ -61,15 +61,22 @@ describe('Reducers', () => {
                 completedAt: 150
             }];
 
+            var updates = {
+                completed: false,
+                completedAt: null
+            };
+
             var action = {
-                type: 'TOGGLE_TODO',
-                id: 1
+                type: 'UPDATE_TODO',
+                id: todos[0].id,
+                updates
             };
 
             var res = reducers.todosReducer(df(todos), df(action));
 
-            expect(res[0].completed).toEqual(false);
-            expect(res[0].completedAt).toEqual(undefined);
+            expect(res[0].completed).toEqual(updates.completed);
+            expect(res[0].completedAt).toEqual(updates.completedAt);
+            expect(res[0].text).toEqual(todos[0].text);
 
         });
 
